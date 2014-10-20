23cac4fce7c5b5e71d3923df454ab3385409f33c
Oliver Stapleton <oliver.stapleton@students.unibe.ch>
2014-10-20 23:42:01 +0200

    Events controller extended with update and edit.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,9 +16,17 @@ class EventsController < ApplicationController
   end
 
   def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render 'edit'
+    end
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
 
   def destroy
