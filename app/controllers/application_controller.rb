cd102e5425d64643ed58bbfdbdae9fc1e3a73d5a
Oliver Stapleton <oliver.stapleton@students.unibe.ch>
2014-11-08 20:22:36 +0100

    Application layout only for logged in users.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
+  layout :layout_by_resource
+
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -27,6 +30,16 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
+  end
+
+  protected
+
+  def layout_by_resource
+    if user_signed_in?
+      "application"
+    else
+      "guest"
+    end
   end
 
 end
