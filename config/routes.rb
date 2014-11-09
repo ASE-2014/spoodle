8c1e3c2aa9a91676c37f48daeaca6e6bb9c56001
Oliver Stapleton <oliver.stapleton@students.unibe.ch>
2014-11-09 20:43:03 +0100

    Generated a welcome view including controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  # You can have the root of your site routed with "root"
+  root 'welcome#index'
+
   resources :events do
     resources :spoodle_dates do
       put :assign, on: :member
@@ -27,8 +30,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
