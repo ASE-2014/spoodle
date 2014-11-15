7122b6f6517c7ce4df33a4d1e500a3ac79a9ecfe
Arun Sittampalam <arun@sittampalam.ch>
2014-11-15 21:00:28 +0100

    Added comments and some minor modifications.

--- app/models/cybercoach_resource.rb
+++ app/models/cybercoach_resource.rb
@@ -1,33 +1,36 @@
 class CybercoachResource
+# Helps map a Cybercoach REST Resource to Ruby objects. Is meant to be extended by subclasses which follow the naming convention CybercoachXyz (eg. CybercoachUser)
 
+  # Extends subclasses
   def self.inherited(base)
-    base.send(:include, InstanceMethods)
     base.extend(ClassMethods)
     base.instance_variable_set(:@resources_base, 'http://diufvm31.unifr.ch:8090/CyberCoachServer/resources')
     resource_name = base.name.split(/(?=[A-Z])/)[1].downcase
     base.instance_variable_set(:@resource_name, resource_name)
   end
 
-  module InstanceMethods
-    def initialize(hash)
-      hash.each do |name, value|
-        instance_variable_set("@#{name}", value)
-        self.class.send(:attr_reader, name)
-      end
+  # Initializes the resource with the attributes given in the hash
+  def initialize(hash)
+    hash.each do |name, value|
+      instance_variable_set("@#{name}", value)
+      self.class.send(:attr_reader, name)
     end
   end
 
   module ClassMethods
+    # Returns all available resource elements as objects
     def get_all
       response = get_rest_response
       response.collect {|hash| self.new(hash)}
     end
 
+    # Select resources using an attribute value pair
     def find_by(key, val)
       all_instances = get_all
       all_instances.select { |elem| elem.send(key) == val}
     end
 
+    # Creates a resource with given id and the attribute value pairs given in the hash
     def create(id, hash)
       uri = "#{@resources_base}/#{@resource_name.pluralize}/#{id}"
       headers = {'Accept' => 'application/json','Content-Type' => 'application/xml'}
@@ -36,6 +39,7 @@ class CybercoachResource
       HTTParty.put(uri, headers: headers, body: payload )
     end
 
+    # Destroys the resource which corresponds to the given id if the username password combination is accepted
     def destroy(id, username, password)
       headers = { "Authorization" => 'Basic ' + Base64.encode64(username + ":" + password),
                   "Accept" => "text/html" }
@@ -48,9 +52,9 @@ class CybercoachResource
       response = HTTParty.get("#{@resources_base}/#{@resource_name.pluralize}/",:headers => {'Accept' => 'application/json'})
       response[@resource_name.pluralize]
     end
-
   end
 
+  # Logs into Cybercoach with the given username password combination
   def self.login(username, password)
     headers = { "Authorization" => 'Basic ' + Base64.encode64(username + ":" + password),
                 "Accept" => "text/html" }
@@ -59,6 +63,7 @@ class CybercoachResource
   end
 
   private
+  # Generates an xml payload which corresponds to the given hash and has the given root argument as root element
   def self.generate_payload(root, hash)
     xmlstring = "<#{root}>"
     hash.each { |key, val| xmlstring << "<#{key}>#{val}</#{key}>"}
