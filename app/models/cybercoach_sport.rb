2f84b7ee2ee37de053be2c1e5ab73d8a8cae868b
Arun Sittampalam <arun@sittampalam.ch>
2014-11-15 22:56:53 +0100

    CybercoachSport determines which attributes of EventData are used from case to case.

--- app/models/cybercoach_sport.rb
+++ app/models/cybercoach_sport.rb
@@ -1,2 +1,16 @@
 class CybercoachSport < CybercoachResource
+
+  # Specifies which data attributes make sense for a sport
+  def data_attributes
+    case self.name
+      when "running"
+        return [:distance]
+      when "soccer"
+        return [:party_1_name, :party_2_name, :winner_name, :score_1, :score_2]
+      when "boxing"
+        return [:party_1_name, :party_2_name, :winner_name]
+      else
+        return []
+    end
+  end
 end
--- app/models/event_data.rb
+++ app/models/event_data.rb
@@ -1,4 +1,8 @@
 class EventData < ActiveRecord::Base
   belongs_to :event
 
+  # Specifies which attributes are actually filled in this case (depends on sport)
+  def attributes
+    self.event.sport.data_attributes
+  end
 end
