6c9d6c262e7ce0002d28bef77a7e6427fccb7640
Urs Zysset <UrsZysset@gmail.com>
2014-11-21 16:10:06 +0100

    Added clean model validators for friendship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,19 @@
 class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, :class_name => 'User'
+
+  validates :user, presence: true
+  validates :friend, presence: true
+
+  validate :not_already_friends
+  validate :not_forever_alone
+
+  def not_already_friends
+    errors.add(:friendship, 'You two are already friends!') if self.user.friends_with self.friend
+  end
+
+  def not_forever_alone
+    errors.add(:friendship, 'You cannot be friends with yourself!') if self.user.eql? self.friend
+  end
+
 end
