577209e23dd704a5e0e1c76e73f1f7430977523a
Arun Sittampalam <arun@sittampalam.ch>
2014-12-09 10:17:53 +0100

    An EventData-Object can be deleted via the events#show view.

--- app/controllers/event_datas_controller.rb
+++ app/controllers/event_datas_controller.rb
@@ -2,7 +2,7 @@ class EventDatasController < ApplicationController
   def new
     @event = Event.find(params[:event_id])
     if not @event.event_data.nil?
-      flash[:error] = "You have already added Event Data. Please use edit."
+      flash[:error] = "You have already added event data. Please use edit."
       redirect_to @event
     else
       @event_data = @event.build_event_data
@@ -13,12 +13,23 @@ class EventDatasController < ApplicationController
     @event = Event.find(params[:event_id])
     @event_data = @event.build_event_data(event_data_params)
     if @event_data.save
-      flash[:success] = "Successfully saved Event Data."
+      flash[:success] = "Successfully saved event data."
       redirect_to @event
     else
-      flash[:error] = "Could not save Event Data."
+      flash[:error] = "Could not save event data."
       redirect_to new_event_event_data_path
     end
+  end
+
+  def destroy
+    @event = Event.find(params[:event_id])
+    @event_data = EventData.find(params[:id])
+    if @event_data.destroy
+      flash[:success] = "Successfully deleted event data."
+    else
+      flash[:error] = "Event data couldn't be deleted!"
+    end
+    redirect_to @event
   end
 
   def event_data_params
