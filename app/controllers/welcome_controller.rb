6a9820811d08902b785a8af8fc2c9ccd46cabce4
Oliver Stapleton <oliver.stapleton@students.unibe.ch>
2014-12-14 21:38:04 +0100

    Readded Twitter, improved social media stuff.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-    @tweets = $twitter.search("spoodleASE", :result_type => 'recent').take(5)
+    @tweets = $twitter.search("spoodleASE", :result_type => 'recent').take(4)
   end
 
 end
